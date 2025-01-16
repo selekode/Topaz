09200a4ea013ba096ca659abe68343997e1e6b8a
selekode <seanlekode@gmail.com>
2025-01-16 13:00:32 +0100

    Spring Initializr and Added Dependencies
    Generated spring boot project and added the necessary dependencies to pom.xml

---
+++ src/main/java/com/selekode/topaz/TopazApplication.java
@@ -0,0 +1,16 @@
+package com.selekode.topaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Topaz | Journal, ToDo List and Habit Tracker
+ * Created by Selekode 16/1/2025
+ */
+@SpringBootApplication
+public class TopazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selekode/topaz/TopazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selekode.topaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
