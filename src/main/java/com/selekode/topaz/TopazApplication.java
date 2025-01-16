27111b70a58ba73ca4d9bc77c432cd846048a207
selekode <seanlekode@gmail.com>
2025-01-16 13:52:25 +0100

    Add JavaFX Webview
    Allows the app to open as a native app with a WebView which shows the frontend

--- src/main/java/com/selekode/topaz/TopazApplication.java
+++ src/main/java/com/selekode/topaz/TopazApplication.java
@@ -2,6 +2,8 @@ package com.selekode.topaz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.selekode.topaz.webview.*;
 /**
  * Topaz | Journal, ToDo List and Habit Tracker
  * Created by Selekode 16/1/2025
@@ -11,6 +13,7 @@ public class TopazApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TopazApplication.class, args);
+		TopazWebview.main(args);
 	}
 
 }
---
+++ src/main/java/com/selekode/topaz/webview/TopazWebview.java
@@ -0,0 +1,51 @@
+package com.selekode.topaz.webview;
+
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.PopupFeatures;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.core.io.ClassPathResource;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+
+public class TopazWebview extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void start(Stage stage) {
+		System.out.println("Launched Website");
+		stage.setTitle("Topaz");
+
+		Image icon = new Image(getClass().getResourceAsStream("/static/assets/brand/topaz-logo.png"));
+
+		stage.getIcons().add(icon);
+
+		WebView webView = new WebView();
+		webView.getEngine().load("http://localhost:8080/login");
+
+		VBox vBox = new VBox(webView);
+		VBox.setVgrow(webView, javafx.scene.layout.Priority.ALWAYS);
+
+		Scene scene = new Scene(vBox, 1010, 600);
+
+		stage.setScene(scene);
+		stage.setMinWidth(1010);
+		stage.show();
+	}
+}
