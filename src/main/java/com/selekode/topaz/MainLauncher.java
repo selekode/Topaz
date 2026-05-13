2540eccad8d182500fb520f54cdb85dbff7cc678
selekode <seanlekode@gmail.com>
2026-05-13 19:03:29 +0200

    Make ready for jpackage distribution

---
+++ src/main/java/com/selekode/topaz/MainLauncher.java
@@ -0,0 +1,8 @@
+package com.selekode.topaz;
+
+public class MainLauncher {
+    public static void main(String[] args) {
+        System.out.println("MAINLAUNCHER LOADING APPLICATION");
+        Application.main(args);
+    }
+}
